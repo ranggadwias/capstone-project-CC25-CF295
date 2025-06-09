d1e2eb4efb1697bfd88c741aa2cceb5a0835e2ff
CICI2505 <162236332+CICI2505@users.noreply.github.com>
2025-06-09 19:29:11 +0700

    Update axiosInstance.js

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://finmate-backend-production.up.railway.app/',
   headers: {
     'Content-Type': 'application/json',
   },
