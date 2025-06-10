91231fa431053c065ee335ed278b54b984b8b521
ranggadwias <ranggadwi783@gmail.com>
2025-06-10 10:22:03 +0700

    change baseURL

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'capstone-project-cc25-cf295-production.up.railway.app',
+  baseURL: 'https://capstone-project-cc25-cf295-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
